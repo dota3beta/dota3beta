abec2de5003a4a54d0bb1f9f8872744ccc8408e4
dota3beta <stillbeta.dota3@gmail.com>
2022-11-30 13:00:55 +0300

    updated all pick

--- pick-hero.js
+++ pick-hero.js
@@ -3,5 +3,6 @@ addEventListener("load", async (event) => {
 });
 
 function ban(hero) {
-	alert(`Вы забанили ${hero}`)
+	alert(`Вы забанили ${hero}, смысла играть больше нет, хотите покинуть матч чтобы найти новую игру?`)
+	window.location.replace("https://dota3beta.github.io/dota3beta/")
 }
